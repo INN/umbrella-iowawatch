d8438bcd6c5e71fc798bd9916d7879705917490e
Ben Keith <ben@benlk.com>
2016-12-05 15:42:02 -0500

    INN_MEMBER and INN_HOSTED

--- wp-content/themes/iowawatch/functions.php
+++ wp-content/themes/iowawatch/functions.php
@@ -3,5 +3,15 @@
  * Child theme functions for Iowa Watch
  */
 
+
+// This site is an INN Member
+if ( ! defined( 'INN_MEMBER' ) ) {
+    define( 'INN_MEMBER', true );
+}
+// This site is hosted by INN
+if ( ! defined( 'INN_HOSTED' ) ) {
+    define( 'INN_HOSTED', true );
+}
+
 // shortcodes in text widgets
 add_filter('widget_text', 'do_shortcode');
