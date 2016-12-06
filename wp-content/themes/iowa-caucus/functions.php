a50566940155ec049f1f15baac3c984eed68fe3d
Ben Keith <ben@benlk.com>
2016-12-05 19:48:45 -0500

    Make iframes max-width 100%; I guess

--- wp-content/themes/iowa-caucus/functions.php
+++ wp-content/themes/iowa-caucus/functions.php
@@ -13,5 +13,7 @@ if ( ! defined( 'INN_HOSTED' ) ) {
 }
 
 // shortcodes in text widgets
-add_filter('widget_text', 'do_shortcode');
-add_filter('widget_text', 'autoembed');
+global $wp_embed;
+add_filter( 'widget_text', 'shortcode_unautop', 8 );
+add_filter( 'widget_text', array( $wp_embed, 'autoembed'), 8 );
+add_filter( 'widget_text', 'do_shortcode', 8 );
